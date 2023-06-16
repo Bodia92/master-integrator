5a34a8c751e90bfc47e5b3e94a18de6c071eb060
Богдан Понамарьов <97220953+Bodia92@users.noreply.github.com>
2023-06-16 16:18:30 +0300

    Add files via upload

---
+++ js/src_js_pages_404_js-src_js_pages_about-us-page_js-src_js_pages_contact-us_js-src_js_pages_cus-401683.js
@@ -0,0 +1,301 @@
+"use strict";
+(self["webpackChunkglivera_webpack_template"] = self["webpackChunkglivera_webpack_template"] || []).push([["src_js_pages_404_js-src_js_pages_about-us-page_js-src_js_pages_contact-us_js-src_js_pages_cus-401683"],{
+
+/***/ "./src/js/components/error-section.js":
+/*!********************************************!*\
+  !*** ./src/js/components/error-section.js ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var ScssComponents_error_section_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ScssComponents/error-section.scss */ "./src/scss/components/error-section.scss");
+
+var errorSection = function errorSection() {};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (errorSection);
+
+/***/ }),
+
+/***/ "./src/js/pages/404.js":
+/*!*****************************!*\
+  !*** ./src/js/pages/404.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _components_error_section__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../components/error-section */ "./src/js/components/error-section.js");
+
+var pageNotFound = function pageNotFound() {
+  (0,_components_error_section__WEBPACK_IMPORTED_MODULE_0__["default"])();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (pageNotFound);
+
+/***/ }),
+
+/***/ "./src/js/pages/about-us-page.js":
+/*!***************************************!*\
+  !*** ./src/js/pages/about-us-page.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import offerBlock from '../components/offer-block';
+// import about from '../components/about';
+
+var aboutUsPage = function aboutUsPage() {
+  // about();
+  // offerBlock();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (aboutUsPage);
+
+/***/ }),
+
+/***/ "./src/js/pages/contact-us.js":
+/*!************************************!*\
+  !*** ./src/js/pages/contact-us.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import contact from '../components/contact';
+
+var contactUs = function contactUs() {
+  // contact();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (contactUs);
+
+/***/ }),
+
+/***/ "./src/js/pages/customer-one.js":
+/*!**************************************!*\
+  !*** ./src/js/pages/customer-one.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import customerOneHero from '../components/customer-one-hero';
+// import customerOneNumbers from '../components/customer-one-numbers';
+// import customerOneContent from '../components/customer-one-content';
+// import customerOneRequests from '../components/customer-one-requests';
+
+var customerOne = function customerOne() {
+  // customerOneHero();
+  // customerOneNumbers();
+  // customerOneContent();
+  // customerOneRequests();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (customerOne);
+
+/***/ }),
+
+/***/ "./src/js/pages/customer-stories.js":
+/*!******************************************!*\
+  !*** ./src/js/pages/customer-stories.js ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import caseStudies from '../components/case-studies';
+// import customerOneRequests from '../components/customer-one-requests';
+
+var customerStories = function customerStories() {
+  // caseStudies();
+  // customerOneRequests();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (customerStories);
+
+/***/ }),
+
+/***/ "./src/js/pages/features.js":
+/*!**********************************!*\
+  !*** ./src/js/pages/features.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import featureList from 'components/feature-list';
+// import getStartedSection from '../components/get-started-section';
+
+var features = function features() {
+  // featureList();
+  // getStartedSection();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (features);
+
+/***/ }),
+
+/***/ "./src/js/pages/home-wix.js":
+/*!**********************************!*\
+  !*** ./src/js/pages/home-wix.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+var homeWix = function homeWix() {};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (homeWix);
+
+/***/ }),
+
+/***/ "./src/js/pages/index.js":
+/*!*******************************!*\
+  !*** ./src/js/pages/index.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import getStarted from '../components/get_started';
+// import workspaceAndWixTabs from '../components/workspace-and-wix-tabs';
+// import findAdvice from '../components/find_advice';
+// import primaryProduct from '../components/primary-products';
+
+var index = function index() {
+  // primaryProduct();
+  // workspaceAndWixTabs();
+  // findAdvice();
+  // getStarted();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (index);
+
+/***/ }),
+
+/***/ "./src/js/pages/pricing-page.js":
+/*!**************************************!*\
+  !*** ./src/js/pages/pricing-page.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import faqSection from '../components/faq-section';
+// import pricingBlock from '../components/pricing-block';
+// import customerOneRequests from '../components/customer-one-requests';
+
+var pricingPage = function pricingPage() {
+  // faqSection();
+  // pricingBlock();
+  // customerOneRequests();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (pricingPage);
+
+/***/ }),
+
+/***/ "./src/js/pages/terms.js":
+/*!*******************************!*\
+  !*** ./src/js/pages/terms.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import generalTerms from '../components/general-terms';
+
+var terms = function terms() {
+  // generalTerms();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (terms);
+
+/***/ }),
+
+/***/ "./src/js/pages/ui.js":
+/*!****************************!*\
+  !*** ./src/js/pages/ui.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+var ui = function ui() {};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ui);
+
+/***/ }),
+
+/***/ "./src/js/pages/wix.js":
+/*!*****************************!*\
+  !*** ./src/js/pages/wix.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import createWebsite from '../components/create-website';
+// import createDesign from '../components/create_design';
+// import customerOneRequests from '../components/customer-one-requests';
+
+var wix = function wix() {
+  // createWebsite();
+  // createDesign();
+  // customerOneRequests();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (wix);
+
+/***/ }),
+
+/***/ "./src/js/pages/workspace.js":
+/*!***********************************!*\
+  !*** ./src/js/pages/workspace.js ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// import workspaceList from 'components/workspace-list';
+// import customerOneRequests from '../components/customer-one-requests';
+
+var workspace = function workspace() {
+  // workspaceList();
+  // customerOneRequests();
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (workspace);
+
+/***/ }),
+
+/***/ "./src/scss/components/error-section.scss":
+/*!************************************************!*\
+  !*** ./src/scss/components/error-section.scss ***!
+  \************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=src_js_pages_404_js-src_js_pages_about-us-page_js-src_js_pages_contact-us_js-src_js_pages_cus-401683.js.map
